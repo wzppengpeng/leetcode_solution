ae12ae240fbfb9f9db9a1693c0ab91d07c3242b9
wzppengpeng <wzpycg@qq.com>
2018-03-19 11:14:17 +0800

    16

---
+++ two_pointers/16.cpp
@@ -0,0 +1,38 @@
+/**
+ * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target. Return the sum of the three integers. You may assume that each input would have exactly one solution.
+
+    For example, given array S = {-1 2 1 -4}, and target = 1.
+
+    The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ */
+
+/**
+ * 必须要考虑到所有的情况，因此双指针的前两个考虑从左侧开始向右走
+ */
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+    if(nums.size() < 3) return 0;
+    int closest = nums[0]+nums[1]+nums[2];
+    sort(nums.begin(), nums.end());
+    for(int first = 0 ; first < nums.size()-2 ; ++first) {
+        if(first > 0 && nums[first] == nums[first-1]) continue;
+        int second = first+1;
+        int third = nums.size()-1;
+        while(second < third) {
+            int curSum = nums[first]+nums[second]+nums[third];
+            if(curSum == target) return curSum;
+            if(abs(target-curSum)<abs(target-closest)) {
+                closest = curSum;
+            }
+            if(curSum > target) {
+                --third;
+            } else {
+                ++second;
+            }
+        }
+    }
+    return closest;
+}
+};
