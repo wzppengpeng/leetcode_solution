37f6975482bd937853c2141b5cc9d64b17ffe463
wzppengpeng <wzpycg@qq.com>
2018-03-21 15:18:27 +0800

    update 31

--- array/31.cpp
+++ array/31.cpp
@@ -21,3 +21,17 @@ public:
         }
     }
 };
+
+void nextPermutation(vector<int>& nums) {
+    int i = nums.size() - 1, k = i;
+    while (i > 0 && nums[i-1] >= nums[i])
+        i--;
+    for (int j=i; j<k; j++, k--)
+        swap(nums[j], nums[k]);
+    if (i > 0) {
+        k = i--;
+        while (nums[k] <= nums[i])
+            k++;
+        swap(nums[i], nums[k]);
+    }
+}
