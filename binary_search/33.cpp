7a5dfad7ae29f317d82df2c4af623562c3c4662d
wzppengpeng <wzpycg@qq.com>
2018-03-22 10:57:29 +0800

    add 33

--- binary_search/33.cpp
+++ binary_search/33.cpp
@@ -38,3 +38,25 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(nums.empty()) return -1;
+        int low = 0, high = nums.size() - 1;
+        while(low < high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[low] == target) return low;
+            if(nums[high] == target) return high;
+            if(nums[mid] >= nums[low]) {
+                if(nums[low] < target && nums[mid] > target) high = mid - 1;
+                else low = mid + 1;
+            } else {
+                if(nums[mid] < target && nums[high] > target) low = mid + 1;
+                else high = mid - 1;
+            }
+        }
+        return nums[low] == target ? low : -1;
+    }
+};
