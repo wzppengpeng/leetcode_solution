255484d4a9965111f2e5d4eece6359e68640e595
wzppengpeng <wzpycg@qq.com>
2018-03-23 17:09:34 +0800

    update 74

--- binary_search/74.cpp
+++ binary_search/74.cpp
@@ -34,3 +34,21 @@ public:
         return false;
     }
 };
+
+/**
+ * 标准答案，但是测出来的反而慢一些...
+ */
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.empty() || matrix[0].empty()) return false;
+        int m = matrix.size(), n = matrix[0].size();
+        int i = 0, j = n - 1;
+        while(i < m && j >= 0) {
+            if(matrix[i][j] == target) return true;
+            else if(matrix[i][j] > target) --j;
+            else ++i;
+        }
+        return false;
+    }
+};
