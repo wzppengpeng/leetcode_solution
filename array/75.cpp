0751b691fafd116b77e188e24b1c2d123a91e032
wzppengpeng <wzpycg@qq.com>
2018-03-25 16:36:18 +0800

    update 75

--- array/75.cpp
+++ array/75.cpp
@@ -32,3 +32,12 @@ public:
         }
     }
 };
+
+void sortColors(vector<int>& nums) {
+    int r=0, w=0, b=0; // label the end of different colors;
+    for(int num: nums){
+        if(num==0) {nums[b++]=2; nums[w++]=1; nums[r++]=0; }
+        else if(num==1) {nums[b++]=2; nums[w++]=1;}
+        else if(num==2) b++;
+    }
+}
