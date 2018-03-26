c373abaca9964f793b0af7d42e0c6d676d3adc72
wzppengpeng <wzpycg@qq.com>
2018-03-26 11:25:22 +0800

    add 90

--- back_tracking/90.cpp
+++ back_tracking/90.cpp
@@ -33,3 +33,34 @@ public:
     }
 };
 
+/**
+ * 可以用动态规划解，但是需要保存之前每一个结束时size的位置
+ */
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> res {{}};
+        if(nums.empty()) return res;
+        std::sort(std::begin(nums), std::end(nums));
+        vector<size_t> index {1};
+        for(size_t i = 0; i < nums.size(); ++i) {
+            auto tmp_size = res.size();
+            if(i > 0 && nums[i] == nums[i - 1]) {
+                for(size_t j = index[i - 1]; j < tmp_size; ++j) {
+                    auto tmp = res[j];
+                    tmp.emplace_back(nums[i]);
+                    res.emplace_back(std::move(tmp));
+                }
+            } else {
+                for(size_t j = 0; j < tmp_size; ++j) {
+                    auto tmp = res[j];
+                    tmp.emplace_back(nums[i]);
+                    res.emplace_back(std::move(tmp));
+                }
+            }
+            index.emplace_back(res.size());
+        }
+        return res;
+    }
+};
+
