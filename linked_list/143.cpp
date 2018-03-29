4ed8d917a10202ee99328f596de8e5dc064230a2
wzppengpeng <wzpycg@qq.com>
2018-03-29 21:22:33 +0800

    update 143

--- linked_list/143.cpp
+++ linked_list/143.cpp
@@ -30,3 +30,58 @@ public:
         nodes[j]->next = nullptr;
     }
 };
+
+
+/**
+ * 用O(1)空间复杂度的方法
+ * 先快慢指针找中间，然后翻转后半部分，然后再将前面和后半部分交叉合并
+ */
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        if(!head || !head->next || !head->next->next) return;
+        auto* mid = find_mid(head);
+        mid = reverse_list(mid);
+        merge(head, mid);
+    }
+
+private:
+    ListNode* find_mid(ListNode* head) {
+        ListNode* low = head, *fast = head;
+        while(fast) {
+            fast = fast->next;
+            if(!fast) {
+                low = low->next;
+                break;
+            }
+            fast = fast->next;
+            low = low->next;
+        }
+        return low;
+    }
+
+    ListNode* reverse_list(ListNode* work) {
+        ListNode *s = nullptr, *p = work, *q = work->next;
+        while(p) {
+            p->next = s;
+            s = p;
+            p = q;
+            if(q) q = q->next;
+        }
+        return s;
+    }
+
+    void merge(ListNode* p, ListNode* q) {
+        ListNode *next_p = nullptr, *next_q = nullptr;
+        while(p && q) {
+            next_p = p->next;
+            next_q = q->next;
+            p->next = q;
+            q->next = next_p;
+            p = next_p;
+            q = next_q;
+        }
+        p->next = q;
+    }
+
+};
