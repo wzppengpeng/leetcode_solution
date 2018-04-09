14e93cd01f622254991ff5ae8810624cc17f9314
wzppengpeng <wzpycg@qq.com>
2018-04-09 14:10:39 +0800

    add 814

---
+++ tree/814.cpp
@@ -0,0 +1,54 @@
+/**
+ * We are given the head node root of a binary tree, where additionally every node's value is either a 0 or a 1.
+
+Return the same tree where every subtree (of the given tree) not containing a 1 has been removed.
+
+(Recall that the subtree of a node X is X, plus every node that is a descendant of X.)
+
+Example 1:
+Input: [1,null,0,0,1]
+Output: [1,null,0,null,1]
+
+Explanation: 
+Only the red nodes satisfy the property "every subtree not containing a 1".
+The diagram on the right represents the answer.
+
+
+Example 2:
+Input: [1,0,1,0,0,0,1]
+Output: [1,null,1,null,1]
+
+
+
+Example 3:
+Input: [1,1,0,1,1,0,1,0]
+Output: [1,1,0,1,1,null,1]
+
+
+
+ */
+
+/**
+ * 使用dfs优先向下判断，而在当前位置，当当前位置为零，且没有左右子树则返回空
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(!root) return root;
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+        if(root->val == 0 && !root->left && !root->right) return nullptr;
+        else return root;
+    }
+};
