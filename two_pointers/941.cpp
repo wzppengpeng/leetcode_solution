787d98f6010ef316374eaca766308d2f2a7c2c89
wzppengpeng <wzpycg@qq.com>
2018-11-19 10:08:34 +0800

    add 941

---
+++ two_pointers/941.cpp
@@ -0,0 +1,39 @@
+/**
+ * Given an array A of integers, return true if and only if it is a valid mountain array.
+
+Recall that A is a mountain array if and only if:
+
+A.length >= 3
+There exists some i with 0 < i < A.length - 1 such that:
+A[0] < A[1] < ... A[i-1] < A[i]
+A[i] > A[i+1] > ... > A[B.length - 1]
+
+Example 1:
+
+Input: [2,1]
+Output: false
+Example 2:
+
+Input: [3,5,5]
+Output: false
+Example 3:
+
+Input: [0,3,2,1]
+Output: true
+ */
+
+
+/**
+ * 使用双指针，一个向前一个向后，最后判断这两者是否相等
+ */
+
+
+class Solution {
+public:
+    bool validMountainArray(vector<int>& A) {
+        int n = A.size(), i = 0, j = n - 1;
+        while (i + 1 < n && A[i] < A[i + 1]) i++;
+        while (j > 0 && A[j - 1] > A[j]) j--;
+        return i > 0 && i == j && j < n - 1;
+    }
+};
