92450f93d5795b9c21c34fc64465e766b2745736
wzppengpeng <wzpycg@qq.com>
2019-08-04 22:56:22 +0800

    update 146

--- design/146.cpp
+++ design/146.cpp
@@ -9,7 +9,7 @@ Could you do both operations in O(1) time complexity?
 
 Example:
 
-LRUCache cache = new LRUCache( 2 /* capacity */ );
+LRUCache cache = new LRUCache( 2 );
 
 cache.put(1, 1);
 cache.put(2, 2);
